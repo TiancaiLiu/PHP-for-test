87d77ab194956f88d8d6203d949b13f08e6e3165
unknown <>
2017-03-14 20:08:45 +0800

    表结构

--- shoppingcard/shop.sql
+++ shoppingcard/shop.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主机: localhost
--- 生成日期: 2017 年 03 月 14 日 11:57
+-- 生成日期: 2017 年 03 月 14 日 12:07
 -- 服务器版本: 5.5.20
 -- PHP 版本: 5.3.10
 
@@ -35,6 +35,27 @@ CREATE TABLE IF NOT EXISTS `shop_cart` (
   `createtime` date NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=51 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `shop_product`
+--
+
+CREATE TABLE IF NOT EXISTS `shop_product` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(225) NOT NULL,
+  `price` float NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- 转存表中的数据 `shop_product`
+--
+
+INSERT INTO `shop_product` (`id`, `title`, `price`) VALUES
+(1, 'Apple 苹果 iPad mini2 16G wifi版 平板电脑', 2199),
+(2, 'Apple 苹果 iPad Air2 32G wifi版 平板电脑', 3599);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
